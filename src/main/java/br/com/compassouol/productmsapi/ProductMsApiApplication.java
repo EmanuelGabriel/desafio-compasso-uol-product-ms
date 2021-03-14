cb426155c8aea96e0c1716e79a6269e7bb34360b
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-03-14 11:37:40 -0300

    first commit

---
+++ src/main/java/br/com/compassouol/productmsapi/ProductMsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.compassouol.productmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMsApiApplication.class, args);
+	}
+
+}
